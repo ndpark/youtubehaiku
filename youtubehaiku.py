e96a5c3155a7052bcfb821d9d88a407bf2970669
Andy Park <hg4park@uwaterloo.ca>
2016-12-07 22:37:21 -0500

    checks for unique links from file

--- youtubehaiku.py
+++ youtubehaiku.py
@@ -4,17 +4,52 @@
 
 import praw
 import webbrowser
+import os
 
 
-my_user_agent = 'youtubeHaikuGetLinks'
-my_client_id = '4C0PjNu3i98OfA'
-my_client_secret = 'AbpDpvUY_XJqobvCj-31BRb3xFw'
+my_user_agent = ''
+my_client_id = ''
+my_client_secret = ''
+links =[]
+
 
 reddit = praw.Reddit(user_agent=my_user_agent, client_id=my_client_id, client_secret=my_client_secret)
 
+
+#Gets submissions from reddit
 for submissions in reddit.subreddit('youtubehaiku').hot(limit=27):
-	links = str(submissions.url)
-	if "www.reddit.com" in links:
+	new_link = str(submissions.url)
+	if "www.reddit.com" in new_link:
 		continue
-	webbrowser.open_new_tab(links)
+	links.append(new_link)
+
+#Open file for reading and gets the list of links from the file
+read_file = open("seen.txt","r+")
+check_lines = read_file.readlines()
+stripped_lines = [i.strip() for i in check_lines]
+
+#if check_lines is empty (file is empty), then open all the links & write the links to the file
+if not check_lines:
+	print('No old links detected, opening all links')
+	for link in links:
+		read_file.write(link+'\n')
+		webbrowser.open(link)
+	read_file.close
+else:
+#if file is not empty, then compare the links from the list and new links from reddit and open the new ones
+	print('Opening new links')
+	unique_links = set(links + stripped_lines)
+	compared_links = set(unique_links).difference(stripped_lines)
+	write_file =open("seen.txt","a")
+	for link in compared_links:
+		webbrowser.open(link)
+		write_file.write(link+'\n')
+	read_file.close
+	
+input('Done, press Enter to exit')
+
+	
+
+
+
 
